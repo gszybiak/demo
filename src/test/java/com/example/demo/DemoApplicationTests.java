b0fca7d231813711adca5c56816fe6c77f5ddca6
gszybiak <g_szybiak@wp.pl>
2025-01-16 22:04:09 +0100

    Added tests for application

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testHello() {
+		String message = "Hello, Jenkins!";
+		assertThat(message).isEqualTo("Hello, Jenkins!");
 	}
 
 }
